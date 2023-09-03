7538980bb114c2f921c5fd3ee351561da88ab9da
ksahh <haseebshaikh25ks@gmail.com>
2023-09-03 16:15:24 +0530

    Gsap animation added and some bug fixed.

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const cursor = document.querySelector(".customCursor");
 const elements = document.querySelectorAll(".elem");
 const videos = document.querySelectorAll("video");
 
-document.addEventListener("mousemove", (e) => {
-    const positionX = e.pageX - cursor.offsetWidth / 2;
-    const positionY = e.pageY - cursor.offsetHeight / 2;
-
+function updateCursorPosition(e) {
+    const positionX = e.clientX - cursor.offsetWidth / 2;
+    const positionY = e.clientY - cursor.offsetHeight / 2;
+  
     cursor.style.transform = `translate(${positionX}px, ${positionY}px)`;
-});
+  }
+  
+  document.addEventListener("mousemove", updateCursorPosition);
 
 
 elements.forEach((element, index) => {
@@ -25,12 +27,56 @@ elements.forEach((element, index) => {
         const offsetX = e.clientX - element.getBoundingClientRect().left;
         const offsetY = e.clientY - element.getBoundingClientRect().top;
 
-        video.style.transform = `translate(${offsetX - 122}px, ${offsetY - 115}px) scale(1)`;        
+        video.style.transform = `translate(${offsetX - 165}px, ${offsetY - 165}px) scale(1)`;        
     });
     
     element.addEventListener("mouseleave", () => {
         cursor.style.display = "block";
-        video.style.transform = `scale(0.5)`; 
+        video.style.transform = `scale(0.7)`; 
         video.style.display = "none";
     });
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+gsap.to(".projects", {
+    scrollTrigger: {
+        trigger: ".fProjects",
+        start: "top top",
+        endTrigger: ".last",
+        end: "bottom bottom",
+        pin: true,
+        scrub: 1,
+    },
+    y : "-505%",
+    ease : Power1
+});
+
+
+let projects = document.querySelectorAll(".projects");
+
+projects.forEach((project, index) => {
+    ScrollTrigger.create({
+        trigger: project,
+        start: "top top",
+        scrub: 1,
+        onUpdate: (self) => {
+            const progress = self.progress;
+            console.log(progress, index);
+        }
+    });
+});
+
+
+gsap.to(".images img", {
+    scrollTrigger: {
+        trigger: ".fProjects",
+        start: "top top",
+        endTrigger: ".last",
+        end: "bottom bottom",
+        scrub: 1,
+    },
+    y: "-505%", // You can adjust the opacity as needed
+    ease: Power3, // Use the correct ease function
 });
